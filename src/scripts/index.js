5943651f11faf81ce9e53b7cee40b974964bf59b
Antoine <fortyonepointsix@gmail.com>
2020-08-01 16:48:33 -0400

    added walletconnect

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,56 @@
 import '../styles/index.scss';
 
+
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+
 console.log('webpack starterkit');
+
+let address
+let chainId
+
+// Create a connector
+const connector = new WalletConnect({
+  bridge: "https://bridge.walletconnect.org", // Required
+  qrcodeModal: QRCodeModal,
+});
+
+// Check if connection is already established
+if (!connector.connected) {
+  // create new session
+  connector.createSession();
+}
+
+// Subscribe to connection events
+connector.on("connect", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Get provided accounts and chainId
+  let { accounts, _chainId } = payload.params[0];
+
+});
+
+connector.on("session_update", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Get updated accounts and chainId
+  const { accounts, chainId } = payload.params[0];
+});
+
+connector.on("disconnect", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Delete connector
+});
+
+function getAddress(){
+  console.log(address)
+}
+console.log("readss")
+console.log(connector)
